b7aab6227101231a79533c5ded342835e4803e53
ManHHin <124098268+ManHHin@users.noreply.github.com>
2023-02-03 20:21:59 -0500

    Add files via upload

---
+++ Book.hpp
@@ -0,0 +1,31 @@
+/*Manhin
+Cs235
+Dr.Tiziana Ligorio
+Project 1
+*/
+#include<string>
+using namespace std;
+
+class Book
+{
+    private:
+        string title;
+        string author;
+        int page_count;
+        bool digital_form;
+
+    public:
+        Book();
+        Book(string title, string author, int page_count);
+        Book(string title, string author, int page_count, bool digital_form);
+        void setTitle(const string& t);
+        string getTitle();
+        void setAuthor(const string& a);
+        string getAuthor();
+        void setPageCount(const int& pc);
+        int getPageCount();
+        void setDigital();
+        bool isDigital();
+
+
+};

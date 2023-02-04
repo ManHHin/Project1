b42fd7993ee53518a7310cb47020dcd915a21c8c
ManHHin <124098268+ManHHin@users.noreply.github.com>
2023-02-03 20:44:18 -0500

    Add files via upload

---
+++ Book.cpp
@@ -0,0 +1,42 @@
+#include "Book.hpp"
+
+Book::Book()
+    :title()
+    ,author()
+    ,page_count()
+    ,digital_form()
+{}
+Book::Book(string title, string author, int page_count, bool digital_form = 0)
+    :title(title)
+    ,author(author)
+    ,page_count(page_count)
+    ,digital_form(digital_form)
+{}
+void Book::setTitle(const string& t){
+    title = t;
+}
+string Book::getTitle(){
+    return title;
+}
+void Book::setAuthor(const string& a){
+    author = a;
+}
+string Book::getAuthor(){
+    return author;
+}
+void Book::setPageCount(const int& pc){
+    if(pc>0)
+        page_count = pc;
+    
+}
+int Book::getPageCount(){
+    if(page_count>0)
+        return page_count;
+    
+}
+void Book::setDigital(){
+    digital_form = 1;
+}
+bool Book::isDigital(){
+    return digital_form;
+}

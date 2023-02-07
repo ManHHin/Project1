38b2d47406a04ae9a6a5382208a99fee04e72755
ManHHin <124098268+ManHHin@users.noreply.github.com>
2023-02-07 13:23:31 -0500

    Update Book.hpp

--- Book.hpp
+++ Book.hpp
@@ -9,39 +9,21 @@ using namespace std;
 class Book
 {
     private:
-    //private member data variableas
         string title;
         string author;
         int page_count;
         bool digital_form;
 
     public:
-    
-    //Default constructor.
-    //Default-initializes all private members. Booleans are default-initialized to False.
         Book();
         Book(string title, string author, int page_count, bool digital_form);
-        
-    
-    //@param  : the title of the Book
-    //@post   : sets the Book's title to the value of the parameter
-      void setTitle(const string& t);
-    //@return : the title of the Book
+        void setTitle(const string& t);
         string getTitle() const;
-    //@param  : the name of the author of the Book
-    //@post   : sets the Book's author to the value of the parameter
         void setAuthor(const string& a);
-    //@return : the author of the Book
         string getAuthor() const;
-    //@param  : a positive integer page count
-    //@pre    : page count > 0 - books cannot have a negative number of pages
-    //@post   : sets the page count to the value of the parameter    
         void setPageCount(const int& pc);
-    //@return : the page count
         int getPageCount() const;
-    //@post   : sets the digital flag to true
         void setDigital();
-    //@return true if the book is available in digital form, false otherwise
         bool isDigital() const;
 
 
